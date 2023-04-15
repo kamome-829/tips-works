a1a07cf915ea3cfd9937d0e8534f09b3f02c85cd
naoki <ueda.191201@gmail.com>
2023-04-15 11:02:17 +0100

    changed example job

--- src/jobExample/jobs.ts
+++ src/jobExample/jobs.ts
@@ -3,35 +3,35 @@ import Job from "../types/jobType";
 export const jobs: Job[] = [
   {
     id: 1,
-    name: "make a job list No.1",
+    name: "make an application No.1",
     description:
       "The job is to make the whole application including managing the infrastructure. Requirements: 1. 2-3 years experience in microservices. 2. proficient in One of these languages: Javascript, Typescript, Java, Python, Go",
     pay: 100,
   },
   {
     id: 2,
-    name: "make a job list No.2",
+    name: "make an application No.2",
     description:
       "The job is to make the whole application including managing the infrastructure. Requirements: 1. 2-3 years experience in microservices. 2. proficient in One of these languages: Javascript, Typescript, Java, Python, Go",
     pay: 200,
   },
   {
     id: 3,
-    name: "make a job list No.3",
+    name: "make an application No.3",
     description:
       "The job is to make the whole application including managing the infrastructure. Requirements: 1. 2-3 years experience in microservices. 2. proficient in One of these languages: Javascript, Typescript, Java, Python, Go",
     pay: 300,
   },
   {
     id: 4,
-    name: "make a job list No.4",
+    name: "make an application No.4",
     description:
       "The job is to make the whole application including managing the infrastructure. Requirements: 1. 2-3 years experience in microservices. 2. proficient in One of these languages: Javascript, Typescript, Java, Python, Go",
     pay: 400,
   },
   {
     id: 5,
-    name: "make a job list No.5",
+    name: "make an application No.5",
     description:
       "The job is to make the whole application including managing the infrastructure. Requirements: 1. 2-3 years experience in microservices. 2. proficient in One of these languages: Javascript, Typescript, Java, Python, Go",
     pay: 500,
